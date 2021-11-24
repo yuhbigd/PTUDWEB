06cee254ae0419ece9a52368a3136984cdc51c05
tran quang huy <ieltsaddict@gmail.com>
2021-11-24 18:26:57 +0700

    this

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Login from './../components/login/Login'
+
 const LoginPage = () => {
     return (
         <div>
--- client/src/routes/AuthRoute.js
+++ client/src/routes/AuthRoute.js
@@ -2,15 +2,15 @@ import React from "react";
 import { Navigate, Outlet} from 'react-router-dom'
 import { useSelector } from "react-redux";
 
-
-
 const AuthRoute = (props) => {
-    const Auth = useSelector(state => state.userRe)
-    console.log(Auth)
-    if (Auth) {
-        return (<Outlet></Outlet>)
+    const Auth =  useSelector(state => state.userRe)
+    const redirect = Auth.auth && Auth.user
+
+    if (true) {
+        return <Outlet></Outlet>
     }else {
-        return (<Navigate to='/login'></Navigate>)
+        return <Navigate to='/login'></Navigate>
     }
 }
+
 export default AuthRoute
