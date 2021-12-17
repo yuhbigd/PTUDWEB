678721e74ebba36ea041562966c033318715c896
Duong Huy <huy6a1@gmail.com>
2021-12-17 14:27:17 +0700

    done

--- client/src/routes/AppRouter.js
+++ client/src/routes/AppRouter.js
@@ -24,7 +24,7 @@ const AppRouter = () => {
     const [themeNum, setThemeNum] = useState( parseInt(localStorage.getItem('theme')))
     
     const [loginRequest, setLoginRequest] = useAsyncFn(async() => {
-        const res = await fetch('http://localhost:3001/login', {
+        const res = await fetch(`${process.env.REACT_APP_BASE_URL}/login`, {
             method: 'GET',
             headers: {
                 'Accept': 'application/json',
@@ -37,7 +37,7 @@ const AppRouter = () => {
     })
 
     const [logOutRequest, setLogOutRequest] = useAsyncFn(async() => {
-        const res = await fetch('http://localhost:3001/logout', {
+        const res = await fetch(`${process.env.REACT_APP_BASE_URL}/logout`, {
             method: 'GET',
             headers: {
                 'Accept': 'application/json',
